c5d17c362cdacfc2676a90d4dfc20cf338e72b65
maheshraja <i.mahesh2004@gmail.com>
2023-12-23 14:42:17 +0000

    datatypes in Javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use Strict"; //Treat all JS Code as new version of JS
+
+let name = "Mahesh"
+let age = 37
+let isLoggedIn = true
+
+// number => 2 ^ 53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value. Representation of Empty Value
+// undefined => variable is declared but not defined.
+// symbol => uniqueness
+// object
+
+console.table([typeof null, typeof age, typeof undefined ])
+// object, number, undefined
